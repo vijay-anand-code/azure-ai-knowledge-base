c1509ed6227966edd14a411bdd221cc95e25fe21
vijay-anand-code <vijay.anand.005@outlook.com>
2026-07-16 09:44:55 +0530

    Add redirects from old source-code paths

--- next.config.js
+++ next.config.js
@@ -1,13 +1,24 @@
 const nextra = require('nextra')
- 
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   defaultShowCopyCode: true,
 })
- 
 module.exports = withNextra({
   reactStrictMode: true,
   trailingSlash: false,
+  async redirects() {
+    return [
+      {
+        source: '/source-code',
+        destination: '/azure-secret-governance/source-code',
+        permanent: true,
+      },
+      {
+        source: '/source-code/:path*',
+        destination: '/azure-secret-governance/source-code/:path*',
+        permanent: true,
+      },
+    ]
+  },
 })
-
